ddd8d8abc44ef39bf3754c0ede27834b2276f21f
abadhikari <adhikari.abhinna@gmail.com>
2024-10-08 21:29:22 -0400

    Remove Audio Player Hover

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,8 +13,6 @@ export default function Header() {
             src="/assets/Debussy - Arabesque No. 1 (Jazz Version) by Jacob Dupre.mp3"
             initialVolume={0.3}
             className={styles.audioButton}
-            link="https://www.youtube.com/watch?v=42FmgGpWQis&list=PLw7XQgISZIIAdy6JOrCQjO1klWhzz7j4V&index=3"
-            redirectTimeSecs={2}
           />
         </div>
         <NavBar />
--- src/components/common/ExtremelySimpleAudioPlayer.tsx
+++ src/components/common/ExtremelySimpleAudioPlayer.tsx
@@ -4,52 +4,27 @@ interface SimpleAudioPlayerProps {
   src: string;
   initialVolume: number;
   className: string;
-  link: string;
-  redirectTimeSecs: number;
 }
 
 export default function SimpleAudioPlayer({
   src,
   initialVolume,
   className,
-  link,
-  redirectTimeSecs,
 }: SimpleAudioPlayerProps) {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [isMuted, setIsMuted] = useState(true);
-  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
 
   const toggleMute = () => {
     const currentAudio = audioRef.current;
     if (currentAudio) {
       currentAudio.muted = !isMuted;
-      currentAudio.play();
+      if (currentAudio.muted) {
+        currentAudio.pause();
+      } else {
+        currentAudio.play();
+      }
     }
     setIsMuted(!isMuted);
-  };
-
-  const mute = () => {
-    const currentAudio = audioRef.current;
-    if (currentAudio) {
-      currentAudio.muted = true;
-    }
-    setIsMuted(true);
-  };
-
-  const startRedirectTimer = () => {
-    // Set a timeout to redirect to link after x seconds
-    timeoutRef.current = setTimeout(() => {
-      window.open(link, '_blank');
-      mute();
-    }, redirectTimeSecs * 1000);
-  };
-
-  const endRedirectTimer = () => {
-    // Clear the timeout before the timeout is complete
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-      timeoutRef.current = null;
-    }
   };
 
   useEffect(() => {
@@ -67,13 +42,7 @@ export default function SimpleAudioPlayer({
         Your browser does not support the audio element.
       </audio>
 
-      <button
-        onClick={toggleMute}
-        type="button"
-        className={className}
-        onMouseEnter={startRedirectTimer}
-        onMouseLeave={endRedirectTimer}
-      >
+      <button onClick={toggleMute} type="button" className={className}>
         {isMuted ? '🔈' : '🔇'}
       </button>
     </div>
